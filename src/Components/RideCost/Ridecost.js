00783ca67aae94813efb54091edb5ae52fa39303
Dipraj Howlader <diprajhowlader2015@gmail.com>
2021-03-21 19:10:30 +0600

    done alsomtt

--- src/Components/RideCost/Ridecost.js
+++ src/Components/RideCost/Ridecost.js
@@ -2,13 +2,30 @@ import React from 'react';
 import './Ridecose.css';
 import fakeData from '../../fakeData/data.json';
 import { useParams } from 'react-router';
+import img from '../../photo/peopleicon.png';
 
 const Ridecost = (props) => {
-    const {id } =useParams();
-    const { name, photo, cost, people } = fakeData[id-1]
+    const { id } = useParams();
+    const { name, photo, cost, people } = fakeData[id - 1];
+    const element = ['one', 'two', 'three'];
     return (
         <div className="ridecost">
-            <h6>{name}</h6>
+            {
+                element.map(() => (
+                    <div className="ride-info">
+                        <div className="first-img">
+                        <img src={photo} alt="" />
+                        </div>
+                        <h6>{name}</h6>
+                        <div className="snd-img">
+                        <img src={img} alt="" />
+                        </div>
+                        <h6>{people}</h6>
+                        <h6>${cost}</h6>
+                        <br/>
+                    </div>
+                ))
+            }
         </div>
     );
 };
--- src/Components/SearchResult/SearchResult.js
+++ src/Components/SearchResult/SearchResult.js
@@ -2,26 +2,34 @@ import React, { useContext } from 'react';
 import Ridecost from '../RideCost/Ridecost';
 import './SearchResult.css';
 import SimpleMap from '../GoogleMap/SimpleMap';
+import { Col, Row } from 'react-bootstrap';
 const SearchResult = () => {
 
 
     return (
 
         <div className="search">
-            <div>
-                <div className="destination">
-                    <h6>Mirpur 1</h6>
-                    <p>to</p>
-                    <h6>Dhanmondi</h6>
-                </div>
-                <div>
-                    <Ridecost></Ridecost>
-                </div>
-            </div>
+            <Row>
+                <Col>
+                    <div className="des-info">
+                        <div className="destination">
+                            <h6>Mirpur 1</h6>
+                            <p>to</p>
+                            <h6>Dhanmondi</h6>
+                        </div>
+                        <div>
+                            <Ridecost></Ridecost>
+                        </div>
+                    </div>
 
-            <div>
-                <SimpleMap></SimpleMap>
-            </div>
+                </Col>
+
+                <Col>
+                    <div className="map">
+                        <SimpleMap></SimpleMap>
+                    </div>
+                </Col>
+            </Row>
         </div>
     );
 };
